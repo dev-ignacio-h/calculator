ef84c804c3a2b1319d4842b9f1dc917b05103936
Ignacio <dev.ignacio.h@gmail.com>
2020-02-15 14:18:44 -0300

    se agregan funciones para manejar simbolos

--- js/calculator.js
+++ js/calculator.js
@@ -2,21 +2,59 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 
-const screen = document.querySelector('.screen');
+const screen = document.querySelector(".screen");
 
 function buttonClick(value) {
   if (isNaN(value)) {
     //not number
-    handleSymbol(value);
+    handleSymbol(value);    
   } else {
     //number
     handleNumber(value);
   }
+  screen.innerText = buffer;
 }
 
-function handleSymbol(symbol){}
-function hanleNumber(numberString){
+function handleSymbol(symbol) {
+  // if (symbol === "C") {
+  //   buffer = "0";
+  //   runningTotal = 0;
+  // }
+  switch (symbol) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "+":
+    case "÷":
+    case "×":
+    case "−":
+      handleMath(symbol);
+      break;
+  }
+}
+
+function handleMath(symbol) {
   if (buffer === '0') {
+    // do nothing
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperator = symbol;
+  buffer = '0';
+}
+
+
+function handleNumber(numberString) {
+  if (buffer === "0") {
     buffer = numberString;
   } else {
     buffer += numberString;
@@ -24,11 +62,11 @@ function hanleNumber(numberString){
 }
 
 function init() {
-  document.querySelector('.calc-buttons')
-    .addEventListener('click', function(event) {
-      console.log(event)
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function(event) {
       buttonClick(event.target.innerText);
-    })
+    });
 }
 
 init();
