2fd3ccbc16351f678d586e9e02486f4dfc7b5883
Ignacio <dev.ignacio.h@gmail.com>
2020-02-17 01:45:04 -0300

    core de calculadora terminado

--- js/calculator.js
+++ js/calculator.js
@@ -5,6 +5,7 @@ let previousOperator = null;
 const screen = document.querySelector(".screen");
 
 function buttonClick(value) {
+  // console.log(value)
   if (isNaN(value)) {
     //not number
     handleSymbol(value);    
@@ -31,6 +32,13 @@ function handleSymbol(symbol) {
       previousOperator = null;
       buffer = runningTotal;
       runningTotal = 0;
+      break;
+    case '←':
+      if (buffer.length === 1) {
+        buffer = '0';
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
       break;
     case "+":
     case "÷":
